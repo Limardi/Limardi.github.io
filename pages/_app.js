a71d352ddfb3eeb54ac92bc3d46f84f44fdf92c0
Vincent <vincentlimardi1234@gmail.com>
2025-03-31 12:53:46 +0800

    Successful build with static export

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css'  // adjust this path if your CSS is elsewhere
+// import '../styles/globals.css'  // adjust this path if your CSS is elsewhere
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
