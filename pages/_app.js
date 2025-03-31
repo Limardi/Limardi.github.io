66f1ad1bdaf8a75708042638381da963abda30fd
Vincent <vincentlimardi1234@gmail.com>
2025-03-31 12:56:02 +0800

    Fix pages router configuration

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 // import '../styles/globals.css'  // adjust this path if your CSS is elsewhere
 
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
-} 
+}
+
+export default MyApp 
